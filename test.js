f958061772451f6a5776394b1342717ba1285b04
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-06 23:23:55 +0300

    commit

---
+++ test.js
@@ -0,0 +1,43 @@
+
+const tehnologies = ["HTML", "CSS", "JS", "React", "Node"];
+
+// const ullist = document.createElement('ul');
+// const ref = document.querySelector('body');
+
+
+
+
+// for (i = 0; i < tehnologies.length; i += 1) {
+//     const liItem = document.createElement('li');
+
+//     liItem.textContent = tehnologies[i];
+
+//     ullist.appendChild(liItem);
+    
+// }
+
+// document.body.prepend(ullist)
+
+//==================================================================================
+
+// const listContent = tehnologies.map(tehnology => `<li>${tehnology}</li>`).join("");
+
+// const list = `<ul>${listContent}</ul>`;
+
+// console.log(list);
+
+// document.body.insertAdjacentHTML("afterbegin",list);
+
+
+//===================================================================================
+
+// const listContent = tehnologies.reduce((markup, tehnology) => markup + `<li>${tehnology}</li>`, ""  )
+
+// const list = `<ul>${listContent}</ul>`;
+ 
+//  document.body.insertAdjacentHTML("afterbegin",list);
+
+//===================================================================================
+
+
+
