f1ce90aeebd69fd5e5140d77834200489e9a5364
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-28 04:52:57 +0300

    commit

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-const number = 10;
+// const number = 10;
 
 
-function parityCheck(number) {
-    if (number % 2 === 0) {
-        console.log('число парне:)')
-    }
-    else {
-        console.log('число не парне!')
-    }
+// function parityCheck(number) {
+//     if (number % 2 === 0) {
+//         console.log('число парне:)')
+//     }
+//     else {
+//         console.log('число не парне!')
+//     }
     
-};
+// };
 
 
 
-parityCheck(number);
+// parityCheck(number);
