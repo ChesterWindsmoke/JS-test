f7e5d85f4cbcb0941205c5b478ac1ca5ec7623ee
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-30 16:56:46 +0300

    update js

--- test.js
+++ test.js
@@ -1,3 +1,24 @@
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
 
-console.table(friends);
+// console.table(friends);
+
+// friends[0] = 'Andrew';
+
+// console.log(friends[0])
+
+// const lastIndex = friends.length - 1;
+// console.log(lastIndex);
+
+// const dirtyMultiply = (array, value) => {
+//     for (i = 0; i < array.length; i += 1) {
+//         array[i] = array[i] * value;
+//     }
+
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// const value = 2;
+
+// dirtyMultiply(numbers, value);
+
+// console.log(numbers);
