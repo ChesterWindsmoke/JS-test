771fe4f71d6890d71956f9029a803e20729f4f0d
Chester <vladislavmarchenko999@gmail.com>
2023-08-08 02:00:30 +0300

    +

---
+++ test.js
@@ -0,0 +1,19 @@
+// const num = ['3','2','1','2']
+
+// function squareDigits(num) {
+//   for (let i = 0; i < num.length; i += 1) {
+//       console.log(parseInt(num[i]) ** 2);
+//   }
+// };
+
+// squareDigits(num);
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+function highAndLow(numbers){
+  // ...
+}
+
+
